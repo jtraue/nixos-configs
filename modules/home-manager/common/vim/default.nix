11fc09e826f1e71da45924956e74f1ca21ca3f17
Jana Traue <jana.traue@cyberus-technology.de>
2022-12-22 16:26:49 +0100

    vim: remove legacy vale styles which depended on niv

--- modules/home-manager/common/vim/default.nix
+++ modules/home-manager/common/vim/default.nix
@@ -19,28 +19,5 @@ in
         })
         pkgs.vale
       ];
-
-      home.file =
-        let
-          sources = import ../../../../nix/sources.nix;
-          valeStyles = pkgs.stdenv.mkDerivation {
-            name = "vale-styles";
-            buildCommand = ''
-              mkdir -p $out/styles
-              cp -r ${sources.vale-style-google}/Google $out/styles
-            '';
-          };
-        in
-        {
-          ".vale.ini" = {
-            text = ''
-              StylesPath = ${valeStyles}/styles
-              MinAlertLevel = warning
-              [*.{org,md}]
-              BasedOnStyles = Google
-            '';
-          };
-        };
-
     };
 }
