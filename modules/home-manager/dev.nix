c730227b4a6ba1ff1f119257353d622135dd4767
Jana Traue <jana.traue@cyberus-technology.de>
2023-06-06 08:48:33 +0200

    qemu: switch to stable version due to 23.05 update

--- modules/home-manager/dev.nix
+++ modules/home-manager/dev.nix
@@ -1,10 +1,7 @@
-{ config, lib, pkgs, pkgs-unstable, ... }:
+{ config, lib, pkgs, ... }:
 
 let
   cfg = config.home-modules.dev;
-  # Use unstable (7.2.0) for fixing this qemu with gdb bug:
-  # qemu-system-x86_64: ../hw/i386/kvm/clock.c:88: kvmclock_current_nsec: Assertion `time.tsc_timestamp <= migration_tsc' failed.
-  qemu = pkgs-unstable.qemu_kvm;
 in
 {
   options.home-modules.dev.enable = lib.mkEnableOption "Enables development environments.";
@@ -18,7 +15,7 @@ in
       gdb
       minicom
       ninja
-      qemu
+      qemu_kvm
       ttylog
     ];
 
