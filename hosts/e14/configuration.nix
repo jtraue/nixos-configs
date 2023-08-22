0a7989854ff23038be625a1c42cc5738fbc43443
Jana Traue <jana.traue@cyberus-technology.de>
2023-08-22 08:28:34 +0200

    Revert "hosts/e14: try to fix gpu issues"
    This reverts commit eb54173ffee3f9fb9aadc671b698c4187b3ec303.
    
    VBox kernel module is broken

--- hosts/e14/configuration.nix
+++ hosts/e14/configuration.nix
@@ -33,9 +33,6 @@
   boot.extraModprobeConfig = "options kvm_intel nested=1";
   boot.kernelParams = [ "intel_iommu=off" ];
 
-  # GPU driver segfaults when connecting HDMI cable and switching to fullscreen. Let's see whether this is gone with the newest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-
   # -- network
   # Note for future me: nmtui can be used for network config on terminal
   networking = {
