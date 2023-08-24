7b24452161f008293e485e5153b30d9050b0cd55
Jana Traue <jtraue@disturbed.systems>
2023-08-24 16:29:34 +0200

    hosts/x13: some tweaks for android/pi development

--- hosts/x13/configuration.nix
+++ hosts/x13/configuration.nix
@@ -5,11 +5,23 @@
   ]
   ++ builtins.attrValues nixosModules;
 
+
+  programs.adb.enable = true;
+  users.users.jtraue.extraGroups = [ "adbusers" ];
+
+  virtualisation.docker.enable = true;
+
+
+
   # Let's see whether this fixes random audio outages
   # as suggested in https://discourse.nixos.org/t/weird-audio-behavior-pipewire-pulseaudio-not-working-sometimes/24124/2
   boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
 
-  services.tailscale.enable = true;
+  services = {
+    tailscale.enable = true;
+    xserver.libinput.mouse.accelSpeed = "0.5";
+  };
   networking.firewall.checkReversePath = "loose"; # for tailscale
 
   nixos-modules.common.enable = true;
