eadc0324853ee54a03b3b8479634362a7144152d
Jana Traue <jana.traue@cyberus-technology.de>
2023-09-22 13:29:10 +0200

    e14: disable virtualbox

--- hosts/e14/configuration.nix
+++ hosts/e14/configuration.nix
@@ -23,9 +23,6 @@
   # pipewire complains about missing xdg-portals (https://www.reddit.com/r/NixOS/comments/y877ou/pipewire_not_working_requires_xdgportals/)
   xdg.portal.enable = true;
 
-  virtualisation.virtualbox.host.enable = true;
-  users.extraGroups.vboxusers.members = [ "jtraue" ];
-
   virtualisation.libvirtd.enable = true;
   programs.dconf.enable = true;
   environment.systemPackages = with pkgs; [ virt-manager gnome.gnome-boxes ];
