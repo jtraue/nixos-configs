9574154e0fbb1f23d965b2c8895340415eac117b
Jana Traue <jana.traue@cyberus-technology.de>
2023-09-24 09:51:08 +0200

    vim: remove modline

--- modules/home-manager/common/vim/nvim/init.lua
+++ modules/home-manager/common/vim/nvim/init.lua
@@ -7,43 +7,44 @@
 
 
 -- Search and replace
-vim.o.ignorecase = true -- make searches with lower case characters case insensative
-vim.o.smartcase  = true -- search is case sensitive only if it contains uppercase chars
-vim.o.inccommand = 'nosplit' -- show preview in buffer while doing find and replace
+vim.o.ignorecase   = true                 -- make searches with lower case characters case insensative
+vim.o.smartcase    = true                 -- search is case sensitive only if it contains uppercase chars
+vim.o.inccommand   = 'nosplit'            -- show preview in buffer while doing find and replace
 
-vim.o.ai = true -- Copy indent from current line when starting a new one
-vim.o.autoread = true -- Set to auto read when a file is changed from the outside
-vim.o.backspace = [[indent,eol,start]] -- allow backspace over indents, etc.
-vim.o.cmdheight = 2 -- number of screen lines for command line
+vim.o.ai           = true                 -- Copy indent from current line when starting a new one
+vim.o.autoread     = true                 -- Set to auto read when a file is changed from the outside
+vim.o.backspace    = [[indent,eol,start]] -- allow backspace over indents, etc.
+vim.o.cmdheight    = 2                    -- number of screen lines for command line
 
 vim.o.colorcolumn  = 80
-vim.o.cursorline   = true -- highlight line in which the cursor is
-vim.o.encoding     = "utf-8" -- Set utf8 as standard encoding
-vim.o.expandtab    = true -- Use spaces instead of tabs
-vim.o.ffs          = [[unix,dos,mac]] -- Use Unix as the standard file type
-vim.o.hidden       = true -- keep buffers open - preserves history
-vim.o.laststatus   = 2 -- always show a status line
-vim.o.lbr          = true -- Linebreak on 500 characters
-vim.o.list         = true -- turn whitespace display on
+vim.o.cursorline   = true                                           -- highlight line in which the cursor is
+vim.o.encoding     = "utf-8"                                        -- Set utf8 as standard encoding
+vim.o.expandtab    = true                                           -- Use spaces instead of tabs
+vim.o.ffs          = [[unix,dos,mac]]                               -- Use Unix as the standard file type
+vim.o.hidden       = true                                           -- keep buffers open - preserves history
+vim.o.laststatus   = 2                                              -- always show a status line
+vim.o.lbr          = true                                           -- Linebreak on 500 characters
+vim.o.list         = true                                           -- turn whitespace display on
 vim.o.listchars    = [[tab:»·,trail:•,nbsp:•,extends:»,precedes:«]] -- display these whitespaces graphically
+vim.o.modelines    = false
 vim.o.nobackup     = true
-vim.o.noerrorbells = true -- No annoying sound on errors
-vim.o.foldenable   = true -- Enable folds so that I get used to them
+vim.o.noerrorbells = true  -- No annoying sound on errors
+vim.o.foldenable   = true  -- Enable folds so that I get used to them
 vim.o.swapfile     = false
 vim.o.visualbell   = false -- again: no sounds
-vim.o.nu           = true -- Show line numbers
-vim.o.ruler        = true -- Always show current position
-vim.o.shiftwidth   = 4 -- 1 tab == 4 spaces
-vim.o.smartindent  = true -- Smart indent
-vim.o.smarttab     = true -- Be smart when using tabs ;)
-vim.o.spell        = true -- enable spell checking
-vim.o.spelllang    = 'en' -- default spellcheck language
-vim.o.tabstop      = 4 -- ...
-vim.o.tm           = 500 -- ...
-vim.o.tw           = 500 -- text width
-vim.o.wildmenu     = true -- enhanced command-line completion mode
-vim.o.wrap         = true -- Wrap lines
-vim.o.writebackup  = true -- make a backup before overwriting a file
+vim.o.nu           = true  -- Show line numbers
+vim.o.ruler        = true  -- Always show current position
+vim.o.shiftwidth   = 4     -- 1 tab == 4 spaces
+vim.o.smartindent  = true  -- Smart indent
+vim.o.smarttab     = true  -- Be smart when using tabs ;)
+vim.o.spell        = true  -- enable spell checking
+vim.o.spelllang    = 'en'  -- default spellcheck language
+vim.o.tabstop      = 4     -- ...
+vim.o.tm           = 500   -- ...
+vim.o.tw           = 500   -- text width
+vim.o.wildmenu     = true  -- enhanced command-line completion mode
+vim.o.wrap         = true  -- Wrap lines
+vim.o.writebackup  = true  -- make a backup before overwriting a file
 
 -- Set UI related options
 -- vim.o.termguicolors   = true
