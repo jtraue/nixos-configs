6dd1be12b972aa73c5fd2eafdbd7166f74421135
Jana Traue <jana.traue@cyberus-technology.de>
2023-12-07 08:44:48 +0100

    vim: switch to ibl v3

--- modules/home-manager/common/vim/nvim/init.lua
+++ modules/home-manager/common/vim/nvim/init.lua
@@ -77,10 +77,7 @@ vim.opt.termguicolors = true
 
 require("bufferline").setup {}
 
-require("indent_blankline").setup {
-    show_current_context = true,
-    show_current_context_start = false,
-}
+require("ibl").setup()
 
 require('gitsigns').setup()
 
